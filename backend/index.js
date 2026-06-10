f2fc4ae2d5ab814bbf3604fcee5dbe885643042d
Sikraam <ikraamse7@gmail.com>
2026-06-10 14:03:27 +0100

    fix: default port 8080

--- backend/index.js
+++ backend/index.js
@@ -100,7 +100,7 @@ app.get('/', (req, res) => {
   res.json({ message: 'API SaaS Leads — Server running ✅' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
